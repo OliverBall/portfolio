b4624f95ecf23bcd70bb4b77a3ff4120e576ab3e
Oliver Ball <113358642+OliverBall@users.noreply.github.com>
2022-09-15 21:56:08 +0100

    Add files via upload

---
+++ flowdev/assets/css/index.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+    document.getElementById("menu").style.display = "block";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("menu").style.display = "none";
+}
